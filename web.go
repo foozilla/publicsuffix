83ddf5c3376e083010145b44e19fd7dc4a573ef1
Erik Dubbelboer <erik@dubbelboer.com>
2015-08-14 16:15:49 +0000

    Fix web.go

--- web.go
+++ web.go
@@ -5,100 +5,12 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
-	"net"
 	"net/http"
-	"net/url"
-	"regexp"
-	"strings"
 
-	. "."
+	"github.com/atomx/publicsuffix/uri"
 )
-
-var (
-	hostEndRegexp = regexp.MustCompile("[^a-z0-9\\.\\-]")
-)
-
-// EffectiveTLDPlusOne returns the effective top level domain plus one more label.
-// For example, "http://www.example.com/foobar" will be "example.com".
-func Example_EffectiveTLDPlusOne(u string) (string, error) {
-	u = strings.ToLower(u)
-
-	if strings.HasPrefix(u, "http%3a%2f%2f") ||
-		strings.HasPrefix(u, "https%3a%2f%2f") ||
-		strings.HasPrefix(u, "%2f%2f") {
-		var err error
-		u, err = url.QueryUnescape(u)
-		if err != nil {
-			return "", err
-		}
-
-		u = strings.ToLower(u)
-	}
-
-	// Trim http:// https:// or // from the start.
-	if strings.HasPrefix(u, "http://") {
-		u = u[len("http://"):]
-	} else if strings.HasPrefix(u, "https://") {
-		u = u[len("https://"):]
-	} else if strings.HasPrefix(u, "//") {
-		u = u[len("//"):]
-	}
-
-	// A TLD+1 needs to be at least 4 characters (g.cn for example).
-	if len(u) < 4 {
-		return "", fmt.Errorf("invalid domain")
-	}
-
-	if u[0] == '.' {
-		u = u[1:]
-	}
-
-	// IPv6?
-	if u[0] == '[' {
-		i := strings.Index(u, "]")
-		if net.ParseIP(u[1:i]) != nil {
-			return u[1:i], nil
-		}
-	}
-
-	// Trim everything after the first non hostname character.
-	ii := hostEndRegexp.FindStringIndex(u)
-
-	if len(ii) > 0 {
-		u = u[0:ii[0]]
-	}
-
-	i := len(u) - 1
-
-	// A TLD+1 needs to be at least 4 characters (g.cn for example).
-	if i < 3 { // Note the - 1 above.
-		return "", fmt.Errorf("invalid domain")
-	}
-
-	// Some web clients really fuck up and somehow end a domain with a '.', remove it.
-	if u[i] == '.' {
-		u = u[0:i]
-
-		// We removed 1 character so check the length again.
-		if i < 4 {
-			return "", fmt.Errorf("invalid domain")
-		}
-	}
-
-	if strings.IndexByte(u, '.') < 1 {
-		return "", fmt.Errorf("invalid domain")
-	}
-
-	// Check if it's an IP.
-	if net.ParseIP(u) != nil {
-		return u, nil
-	}
-
-	return EffectiveTLDPlusOne(u)
-}
 
 type data struct {
 	Hostname   string
@@ -143,7 +55,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%22s | %s\n", r.RemoteAddr, d.Hostname)
 
 	var err error
-	d.TLDPlusOne, err = Example_EffectiveTLDPlusOne(d.Hostname)
+	d.TLDPlusOne, err = uri.EffectiveTLDPlusOne(d.Hostname)
 
 	if err != nil {
 		d.Error = err.Error()
